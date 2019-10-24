00ace1c0a7ab28474918f714dd4a41c9161c3bed
Stephen Howe <Stevehowe10@gmail.com>
2019-10-23 18:21:16 -0600

    refresh button

--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -116,33 +116,7 @@ module.exports = function(app) {
     })
   })
 
-  app.delete("/delete/", function(req, res) {
-    db.Prices.destroy()
-      .then(function(dbPrices) {
-        res.json(dbPrices);
-      });
-  });
-  
 }
 
 
-// const apis = ["https://api.gemini.com/v1/pubticker/btcusd", "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"]
-// const newApis = String.valueOf(apis)
-// console.log(newApis)
-// Promise.all(newApis,(function(api){    
-    
-//     return new Promise (function(resolve, reject){
-//         axios.get(apis).then(function(apiData){
-//             resolve (apiData.data)
-//         })
-//         .catch(function(err){
-//             reject(err)
-//         })
-//     })
-// }))
-// .then(function(values){
-//     var newValues = String.valueOf(values)
-//             res.json(newValues)
-//         }).catch(function(err){
-//             res.send(`Error Thrown: ${err.message}`)
-//         })
+
